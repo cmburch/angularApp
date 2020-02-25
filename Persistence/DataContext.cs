56c8cb924922ca1396ee442d3c0d837f4391bb04
Charles Burch <georgia_beast89@yahoo.com>
2020-02-25 18:11:44 -0500

    added activity entity and Dbset

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -8,9 +8,10 @@ namespace Persistence
     {
         public DataContext(DbContextOptions options) : base(options)
         {
-            
+        
         }
         public DbSet<Value> Values { get; set; }
+        public DbSet<Activity> Activities { get; set; }
 
                 protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -19,6 +20,7 @@ namespace Persistence
             builder.Entity<Value>()
                 .HasData(
                     new Value { Id = 1, Name = "Value 101" },
+
                     new Value { Id = 2, Name = "Value 102" },
                     new Value { Id = 3, Name = "Value 103" }
                 );
