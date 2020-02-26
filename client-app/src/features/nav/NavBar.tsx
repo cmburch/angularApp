4ab2ff9cfbfb7f1e35fc53f662b894e4157f387e
Charles Burch <georgia_beast89@yahoo.com>
2020-02-26 14:28:40 -0500

    add Activity dashboard,list,and form for starter UI crud

--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
  import { Menu, Container, Button } from 'semantic-ui-react';
 
+  interface IProps {
+   openCreateForm: () => void;
+ }
 
-  const NavBar = () => {
+  const NavBar: React.FC<IProps> = ({openCreateForm}) => {
    return (
      <Menu fixed='top' inverted>
        <Container>
@@ -12,7 +15,7 @@ import React from 'react';
          </Menu.Item>
          <Menu.Item name='Activities' />
          <Menu.Item>
-             <Button positive content='Create Activity' />
+             <Button onClick={openCreateForm} positive content='Create Activity' />
          </Menu.Item>
        </Container>
      </Menu>
