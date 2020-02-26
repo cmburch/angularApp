acb27e31b21662fd12f23bbea6d08d9ac85f2c04
Charles Burch <georgia_beast89@yahoo.com>
2020-02-26 02:04:59 -0500

    added the edit handler

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -35,5 +35,12 @@
         {
             return await _mediator.Send(command);
         }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
+        {
+            command.Id = id;
+            return await _mediator.Send(command);
+        }
      }
  } 
