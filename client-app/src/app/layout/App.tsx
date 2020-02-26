49244f971c8bc3dfc8d9adbd8a5510abcd977591
Charles Burch <georgia_beast89@yahoo.com>
2020-02-26 11:24:53 -0500

    added a navigation bar

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import { Header, Icon, List } from 'semantic-ui-react'
 import { IActivity } from '../models/activity';
+import NavBar from '../../features/nav/NavBar';
 
 const App = () => {
   const [activities, setActivities] = useState<IActivity[]>([]);
@@ -15,10 +16,7 @@ const App = () => {
   }, []);
   return (
     <div>
-      <Header as="h2">
-        <Icon name="users" />
-        <Header.Content>Reactor</Header.Content>
-      </Header>
+      <NavBar />
       <List>
         {activities.map((activity: any) => (
           <List.Item key={activity.id}>{activity.title}</List.Item>
